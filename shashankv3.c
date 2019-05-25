0e0ad9cfe1cfda7ce6a070d305ae58ba1c28e4c5
gowdavsg <50978705+gowdavsg@users.noreply.github.com>
2019-05-25 12:03:55 +0530

    Add files via upload

--- shashankv3.c
+++ shashankv3.c
@@ -3,22 +3,28 @@ struct fract
 {
 	int n,d;
 };
-int add(int n1,int d1,int n2,int d2,int n,int d)
+int gcd(struct fract z)
+{
+        int i,a;
+         for(i=1;i<=z.n&&i<=z.d;i++)
+        {
+                if((z.n)%i==0&&(z.d)%i==0)
+                {
+                        a=i;
+                }
+        }
+        return a;
+}
+
+int add(struct fract x,struct fract y,struct fract *z)
 {
 	int i,a;
-	n=(n1*d2)+(n2*d1);
-	d=d1*d2;
-	for(i=1;i<=n&&i<=d;i++)
-	{
-		if(n%i==0&&d%i==0)
-		{
-			a=i;
-		}
-	}
-	n=n/a;
-	d=d/a;
-	printf("\n Sum=%d/%d",n,d);
-}			
+	z->n=((x.n)*(y.d))+((y.n)*(x.d));
+	z->d=(x.d)*(y.d);
+	a=gcd(*z);
+	z->n=(z->n)/a;
+	z->d=(z->d)/a;
+}		
 int main()
 {
 	struct fract x,y,z;
@@ -26,5 +32,7 @@ int main()
 	scanf("%d %d",&x.n,&x.d);
 	printf("\n Enter numerator and denominator of num2:");
 	scanf("%d %d",&y.n,&y.d);
-	add(x.n,x.d,y.n,y.d,z.n,z.d);
+	add(x,y,&z);
+	printf("\n Sum=%d/%d",z.n,z.d);
 }
+
